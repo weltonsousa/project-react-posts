ece7d3e802af5889a7a0d0f91d2627b33a25c2e7
weltonsousa <weltonsousa@gmail.com>
2022-02-01 14:39:40 -0300

    change component button class for function

--- src/components/Button/index.jsx
+++ src/components/Button/index.jsx
@@ -1,19 +1,7 @@
-import { Component } from "react/cjs/react.development"
-
 import './styles.css';
 
-export class Button extends Component {
-    render() {
-        const {text, onClick, disabled} = this.props;
-
-        return(
-            <button
-             disabled={disabled}
-             className='button'
-             onClick={onClick}>
-                 {text}             
-             </button>
-        );     
-    }
-
-}
+export const Button = ({text, onClick, disabled = false}) => (
+    <button className="button" onClick={onClick} disabled={disabled}>
+         {text}
+     </button>
+);
